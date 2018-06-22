5a9283b556a82b1130882b9ed930eff229f1fdf4
shubhamRelus <40175978+shubhamRelus@users.noreply.github.com>
2018-06-22 13:47:49 -0400

    Update main.tf

--- Terraform-Code/main.tf
+++ Terraform-Code/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
 	region 				= "us-east-1"
-	access_key 			= "${var.ACCESS_KEY_ID}"
-	secret_key 			= "${var.SECRET_ACCESS_KEY_ID}"
+	access_key 			= "${var.access_key}"
+	secret_key 			= "${var.secret_access_key}"
 	profile 			= "default"
 }
 
